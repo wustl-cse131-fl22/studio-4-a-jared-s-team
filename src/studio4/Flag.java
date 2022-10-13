0fd437b5c1aa5c8b6767f53bdb9d94d27b9bd90a
jaredallen <jaredallen@jareds-air.dhcp.wustl.edu>
2022-10-13 14:02:44 -0500

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,48 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		JFileChooser chooser = new JFileChooser("resources");
+		chooser.showOpenDialog(null);
+		File f = new File(chooser.getSelectedFile().getPath());
+		Scanner in = new Scanner(f); //making Scanner with a File
+		StdDraw.setXscale(0, 16);
+		StdDraw.setYscale(0, 16);
+		String shapeType = "rectangle";
+		boolean isFilled = true;
+		double parameter1 = 8;
+		double parameter2 = 8;
+		double parameter3 = 2;
+		double parameter4 = 2;
+		int redComponent = 255;
+		int blueComponent = 255;
+		int greenComponent = 0;
 		
+		while (in.hasNextLine() == true) {
+			shapeType = in.next();
+			redComponent = in.nextInt();
+			blueComponent = in.nextInt();
+			greenComponent = in.nextInt();
+			isFilled = in.nextBoolean();
+			parameter1 = in.nextDouble();
+			parameter2 = in.nextDouble();
+			parameter3 = in.nextDouble();
+			parameter4 = in.nextDouble();
+		}
+		
+		if (shapeType.equals("rectangle") && isFilled == true){
+			StdDraw.setPenColor(redComponent, blueComponent, greenComponent);
+			StdDraw.filledRectangle(parameter1,parameter2, parameter3, parameter4);
+				
+		}
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,41 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = "string";
+		boolean isFilled = false;
+		double parameter1 = 0;
+		double parameter2 = 0;
+		double parameter3 = 0;
+		double parameter4 = 0;
+		int redComponent = 0;
+		int blueComponent = 0;
+		int greenComponent = 0;
+		
+		while (in.hasNextLine() == true) {
+			shapeType = in.next();
+			redComponent = in.nextInt();
+			blueComponent = in.nextInt();
+			greenComponent = in.nextInt();
+			isFilled = in.nextBoolean();
+			parameter1 = in.nextDouble();
+			parameter2 = in.nextDouble();
+			parameter3 = in.nextDouble();
+			parameter4 = in.nextDouble();
+			
+			
+			
+		}
+	
+		
+		if (shapeType.equals("rectangle") && isFilled == true){
+			StdDraw.setPenColor(redComponent, blueComponent, greenComponent);
+			StdDraw.filledRectangle(parameter1,parameter2, parameter3, parameter4);
+				
+		}
+		
+		
+		
+		
 		
 	}
 }
